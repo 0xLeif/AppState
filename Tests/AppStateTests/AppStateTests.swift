c7eb37e2288d3a4ccb9c9453345ce9999ac4ec8a
Leif <zmeriksen@icloud.com>
2023-11-07 17:05:09 -0700

    Organize files, clean up dependency, update comment

--- Tests/AppStateTests/AppStateTests.swift
+++ Tests/AppStateTests/AppStateTests.swift
@@ -2,27 +2,7 @@ import SwiftUI
 import XCTest
 @testable import AppState
 
-protocol Networking {
-    func fetch()
-}
-
-struct NetworkService: Networking {
-    func fetch() {
-        fatalError()
-    }
-}
-
-struct MockNetworking: Networking {
-    func fetch() {
-
-    }
-}
-
-extension Application {
-    var networking: Dependency<Networking> {
-        dependency(NetworkService())
-    }
-
+fileprivate extension Application {
     var isLoading: State<Bool> {
         state(initial: false)
     }
@@ -34,13 +14,9 @@ extension Application {
     var colors: State<[String: CGColor]> {
         state(initial: ["primary": CGColor(red: 1, green: 0, blue: 1, alpha: 1)])
     }
-
-    var storedValue: StoredState<Int?> {
-        storedState(initial: nil, id: "storedValue")
-    }
 }
 
-class ExampleViewModel: ObservableObject {
+fileprivate class ExampleViewModel: ObservableObject {
     @AppState(\.username) var username
 
     func testPropertyWrapper() {
@@ -48,25 +24,7 @@ class ExampleViewModel: ObservableObject {
     }
 }
 
-class ExampleStoringViewModel: ObservableObject {
-    @StoredState(\.storedValue) var count
-
-    func testPropertyWrapper() {
-        count = 27
-        _ = TextField(
-            value: $count,
-            format: .number,
-            label: { Text("Count") }
-        )
-    }
-}
-
-struct ExampleStoredValue {
-    @StoredState(\.storedValue) var count
-}
-
-struct ExampleView: View {
-    @AppDependency(\.networking) var networking
+fileprivate struct ExampleView: View {
     @AppState(\.username) var username
     @AppState(\.isLoading) var isLoading
 
@@ -74,7 +32,6 @@ struct ExampleView: View {
 
     func testPropertyWrappers() {
         username = "Hello, ExampleView"
-        networking.fetch()
         _ = Toggle(isOn: $isLoading) {
             Text("Is Loading")
         }
@@ -83,7 +40,7 @@ struct ExampleView: View {
 
 final class AppStateTests: XCTestCase {
     override class func tearDown() {
-        Application.logger.debug("\(Application.description)")
+        Application.logger.debug("AppStateTests \(Application.description)")
     }
 
     override func tearDown() {
@@ -102,29 +59,9 @@ final class AppStateTests: XCTestCase {
         XCTAssertEqual(Application.state(\.username).value, "0xL")
     }
 
-    func testDependency() {
-        let networkingOverride = Application.override(\.networking, with: MockNetworking())
-
-        let mockNetworking = Application.dependency(\.networking)
-
-        XCTAssertNotNil(mockNetworking as? MockNetworking)
-
-        mockNetworking.fetch()
-
-        networkingOverride.cancel()
-
-        let networkingService = Application.dependency(\.networking)
-
-        XCTAssertNotNil(networkingService as? NetworkService)
-    }
-
     func testPropertyWrappers() {
         let exampleView = ExampleView()
 
-        let networkingOverride = Application.override(\.networking, with: MockNetworking())
-        defer { networkingOverride.cancel() }
-
-        XCTAssertNotNil(exampleView.networking as? MockNetworking)
         XCTAssertEqual(exampleView.username, "Leif")
 
         exampleView.testPropertyWrappers()
@@ -138,37 +75,5 @@ final class AppStateTests: XCTestCase {
         viewModel.username = "Hello, ViewModel"
 
         XCTAssertEqual(viewModel.username, "Hello, ViewModel")
-    }
-
-    func testStoredState() {
-        XCTAssertNil(Application.storedState(\.storedValue).value)
-
-        let storedValue = ExampleStoredValue()
-
-        XCTAssertEqual(storedValue.count, nil)
-
-        storedValue.count = 1
-
-        XCTAssertEqual(storedValue.count, 1)
-
-        storedValue.count = nil
-
-        XCTAssertNil(Application.storedState(\.storedValue).value)
-    }
-
-    func testStoringViewModel() {
-        XCTAssertNil(Application.storedState(\.storedValue).value)
-
-        let viewModel = ExampleStoringViewModel()
-
-        XCTAssertEqual(viewModel.count, nil)
-
-        viewModel.count = 2
-
-        XCTAssertEqual(viewModel.count, 2)
-
-        Application.remove(storedState: \.storedValue)
-
-        XCTAssertNil(viewModel.count)
     }
 }
