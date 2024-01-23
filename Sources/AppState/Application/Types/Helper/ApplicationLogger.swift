07553bc262425419d9322d4ae7877e7aa9f9cf06
Zach <zmeriksen@icloud.com>
2024-01-22 20:00:10 -0700

    Simplify Linux and Windows Logging API (#79)
    * Simplify Linux and Windows Logging API
    
    * Fix compile error
    
    * Fix error mismatch
    
    * Fix error logger issue

--- Sources/AppState/Application/Types/Helper/ApplicationLogger.swift
+++ Sources/AppState/Application/Types/Helper/ApplicationLogger.swift
@@ -1,53 +1,23 @@
 #if os(Linux) || os(Windows)
 open class ApplicationLogger {
-    open func debug(
-        _ message: String,
-        _ fileID: StaticString = #fileID,
-        _ function: StaticString = #function,
-        _ line: Int = #line,
-        _ column: Int = #column
-    ) {
-        debug(
-            { message },
-            fileID,
-            function,
-            line,
-            column
-        )
+    open func debug(_ message: String) {
+        debug { message }
     }
 
-    open func debug(
-        _ message: () -> String,
-        _ fileID: StaticString = #fileID,
-        _ function: StaticString = #function,
-        _ line: Int = #line,
-        _ column: Int = #column
-    ) {
-        Application.log(
-            debug: message,
-            fileID: fileID,
-            function: function,
-            line: line,
-            column: column
-        )
+    open func debug(_ message: () -> String) {
+        print(message())
     }
 
-    open func error(
-        _ error: Error,
-        message: String,
-        _ fileID: StaticString = #fileID,
-        _ function: StaticString = #function,
-        _ line: Int = #line,
-        _ column: Int = #column
-    ) {
-        Application.log(
-            error: error,
-            message: message,
-            fileID: fileID,
-            function: function,
-            line: line,
-            column: column
-        )
+    open func error(_ error: Error, message: String? = nil) {
+        guard let message else {
+            return print("Error: \(error.localizedDescription)")
+        }
+
+        print("\(message) (Error: \(error.localizedDescription))")
+    }
+
+    open func error(_ message: String) {
+        print("Error: \(message)")
     }
 }
 #endif
