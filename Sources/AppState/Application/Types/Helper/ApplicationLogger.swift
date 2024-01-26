1c34fce594a88994dc8288d96a916edb6dc7ed56
Zach <zmeriksen@icloud.com>
2024-01-25 17:57:00 -0700

    Add documentation (#87)

--- Sources/AppState/Application/Types/Helper/ApplicationLogger.swift
+++ Sources/AppState/Application/Types/Helper/ApplicationLogger.swift
@@ -1,13 +1,22 @@
 #if os(Linux) || os(Windows)
+/// `ApplicationLogger` is a class that provides logging functionalities for Linux and Windows operating systems.
 open class ApplicationLogger {
+    /// Prints a debug message.
+    /// - Parameter message: The message to be logged.
     open func debug(_ message: String) {
         debug { message }
     }
 
+    /// Prints a debug message.
+    /// - Parameter message: A closure that returns the message to be logged.
     open func debug(_ message: () -> String) {
         print(message())
     }
 
+    /// Logs an error message.
+    /// - Parameters:
+    ///   - error: The error that occurred.
+    ///   - message: An optional custom message to accompany the error.
     open func error(_ error: Error, message: String? = nil) {
         guard let message else {
             return print("Error: \(error.localizedDescription)")
@@ -16,6 +25,8 @@ open class ApplicationLogger {
         print("\(message) (Error: \(error.localizedDescription))")
     }
 
+    /// Logs an error message.
+    /// - Parameter message: The error message to be logged.
     open func error(_ message: String) {
         print("Error: \(message)")
     }
--- Sources/AppState/Application/Types/Helper/MutableApplicationState.swift
+++ Sources/AppState/Application/Types/Helper/MutableApplicationState.swift
@@ -1,5 +1,11 @@
+/**
+A protocol that represents a mutable application state.
+
+This protocol defines a type, `Value`, and a mutable property, `value`, of that type. It serves as the blueprint for any type that needs to represent a mutable state within an application.
+*/
 public protocol MutableApplicationState {
     associatedtype Value
 
+    /// The actual value that this state holds. It can be both retrieved and modified.
     var value: Value { get set }
 }
