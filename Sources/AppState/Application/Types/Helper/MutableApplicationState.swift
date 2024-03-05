3aa91d366d0179b270e364616d0527f6d9974af4
Leif <zmeriksen@icloud.com>
2024-03-04 18:18:35 -0700

    Update State to work with all tpyes of state

--- Sources/AppState/Application/Types/Helper/MutableApplicationState.swift
+++ Sources/AppState/Application/Types/Helper/MutableApplicationState.swift
@@ -6,6 +6,12 @@ This protocol defines a type, `Value`, and a mutable property, `value`, of that 
 public protocol MutableApplicationState {
     associatedtype Value
 
+    static var emoji: Character { get }
+
     /// The actual value that this state holds. It can be both retrieved and modified.
     var value: Value { get set }
+}
+
+extension MutableApplicationState {
+    static var emoji: Character { "❓" }
 }
