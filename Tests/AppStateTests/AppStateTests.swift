496df33c2907637a48239e4d8034017f31c11aa0
Leif <zmeriksen@icloud.com>
2024-09-19 17:38:49 -0600

    Add back another test

--- Tests/AppStateTests/AppStateTests.swift
+++ Tests/AppStateTests/AppStateTests.swift
@@ -1,116 +1,116 @@
-//import Foundation
-//#if !os(Linux) && !os(Windows)
-//import SwiftUI
-//#endif
-//import XCTest
-//@testable import AppState
-//
-//fileprivate extension Application {
-//    var isLoading: State<Bool> {
-//        state(initial: false)
-//    }
-//    
-//    var username: State<String> {
-//        state(initial: "Leif")
-//    }
-//    
-//    var date: State<Date> {
-//        state(initial: Date())
-//    }
-//    
-//    var colors: State<[String: String]> {
-//        state(initial: ["primary": "#A020F0"])
-//    }
-//}
-//
-//@MainActor
-//fileprivate class ExampleViewModel {
-//    @AppState(\.username) var username
-//    
-//    func testPropertyWrapper() {
-//        username = "Hello, ExampleView"
-//    }
-//}
-//
-//#if !os(Linux) && !os(Windows)
-//extension ExampleViewModel: ObservableObject { }
-//
-//fileprivate struct ExampleView: View {
-//    @AppState(\.username) var username
-//    @AppState(\.isLoading) var isLoading
-//    
-//    func testPropertyWrappers() {
-//        username = "Hello, ExampleView"
-//        #if !os(Linux) && !os(Windows)
-//        _ = Toggle(isOn: $isLoading) {
-//            Text("Is Loading")
-//        }
-//        #endif
-//    }
-//    
-//    var body: some View { EmptyView() }
-//}
-//#else
-//@MainActor
-//fileprivate struct ExampleView {
-//    @AppState(\.username) var username
-//    @AppState(\.isLoading) var isLoading
-//    
-//    func testPropertyWrappers() {
-//        username = "Hello, ExampleView"
-//    }
-//}
-//#endif
-//
-//@MainActor
-//final class AppStateTests: XCTestCase {
-//    override func setUp() async throws {
-//        Application.logging(isEnabled: true)
-//    }
-//    
-//    override func tearDown() async throws {
-//        let applicationDescription = Application.description
-//        Application.logger.debug("AppStateTests \(applicationDescription)")
-//        
-//        var username: Application.State = Application.state(\.username)
-//        
-//        username.value = "Leif"
-//    }
-//    
-//    func testState() async {
-//        var appState: Application.State = Application.state(\.username)
-//        
-//        XCTAssertEqual(appState.value, "Leif")
-//        
-//        appState.value = "0xL"
-//        
-//        XCTAssertEqual(appState.value, "0xL")
-//        XCTAssertEqual(Application.state(\.username).value, "0xL")
-//    }
-//    
-//    func testStateClosureCachesValueOnGet() async {
-//        let dateState: Application.State = Application.state(\.date)
-//        
-//        let copyOfDateState: Application.State = Application.state(\.date)
-//        
-//        XCTAssertEqual(copyOfDateState.value, dateState.value)
-//    }
-//    
-//    func testPropertyWrappers() {
-//        let exampleView = ExampleView()
-//        
-//        XCTAssertEqual(exampleView.username, "Leif")
-//        
-//        exampleView.testPropertyWrappers()
-//        
-//        XCTAssertEqual(exampleView.username, "Hello, ExampleView")
-//        
-//        let viewModel = ExampleViewModel()
-//        
-//        XCTAssertEqual(viewModel.username, "Hello, ExampleView")
-//        
-//        viewModel.username = "Hello, ViewModel"
-//        
-//        XCTAssertEqual(viewModel.username, "Hello, ViewModel")
-//    }
-//}
+import Foundation
+#if !os(Linux) && !os(Windows)
+import SwiftUI
+#endif
+import XCTest
+@testable import AppState
+
+fileprivate extension Application {
+    var isLoading: State<Bool> {
+        state(initial: false)
+    }
+    
+    var username: State<String> {
+        state(initial: "Leif")
+    }
+    
+    var date: State<Date> {
+        state(initial: Date())
+    }
+    
+    var colors: State<[String: String]> {
+        state(initial: ["primary": "#A020F0"])
+    }
+}
+
+@MainActor
+fileprivate class ExampleViewModel {
+    @AppState(\.username) var username
+    
+    func testPropertyWrapper() {
+        username = "Hello, ExampleView"
+    }
+}
+
+#if !os(Linux) && !os(Windows)
+extension ExampleViewModel: ObservableObject { }
+
+fileprivate struct ExampleView: View {
+    @AppState(\.username) var username
+    @AppState(\.isLoading) var isLoading
+    
+    func testPropertyWrappers() {
+        username = "Hello, ExampleView"
+        #if !os(Linux) && !os(Windows)
+        _ = Toggle(isOn: $isLoading) {
+            Text("Is Loading")
+        }
+        #endif
+    }
+    
+    var body: some View { EmptyView() }
+}
+#else
+@MainActor
+fileprivate struct ExampleView {
+    @AppState(\.username) var username
+    @AppState(\.isLoading) var isLoading
+    
+    func testPropertyWrappers() {
+        username = "Hello, ExampleView"
+    }
+}
+#endif
+
+@MainActor
+final class AppStateTests: XCTestCase {
+    override func setUp() async throws {
+        Application.logging(isEnabled: true)
+    }
+    
+    override func tearDown() async throws {
+        let applicationDescription = Application.description
+        Application.logger.debug("AppStateTests \(applicationDescription)")
+        
+        var username: Application.State = Application.state(\.username)
+        
+        username.value = "Leif"
+    }
+    
+    func testState() {
+        var appState: Application.State = Application.state(\.username)
+        
+        XCTAssertEqual(appState.value, "Leif")
+        
+        appState.value = "0xL"
+        
+        XCTAssertEqual(appState.value, "0xL")
+        XCTAssertEqual(Application.state(\.username).value, "0xL")
+    }
+    
+    func testStateClosureCachesValueOnGet() {
+        let dateState: Application.State = Application.state(\.date)
+        
+        let copyOfDateState: Application.State = Application.state(\.date)
+        
+        XCTAssertEqual(copyOfDateState.value, dateState.value)
+    }
+    
+    func testPropertyWrappers() {
+        let exampleView = ExampleView()
+        
+        XCTAssertEqual(exampleView.username, "Leif")
+        
+        exampleView.testPropertyWrappers()
+        
+        XCTAssertEqual(exampleView.username, "Hello, ExampleView")
+        
+        let viewModel = ExampleViewModel()
+        
+        XCTAssertEqual(viewModel.username, "Hello, ExampleView")
+        
+        viewModel.username = "Hello, ViewModel"
+        
+        XCTAssertEqual(viewModel.username, "Hello, ViewModel")
+    }
+}
