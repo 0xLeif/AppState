a2644542ba700f61af91e821bc97c92432c97fb0
Leif <zmeriksen@icloud.com>
2024-09-18 19:02:06 -0600

    Update AppLogger to struct from class

--- Sources/AppState/Application/Types/Helper/ApplicationLogger.swift
+++ Sources/AppState/Application/Types/Helper/ApplicationLogger.swift
@@ -1,34 +1,48 @@
 #if os(Linux) || os(Windows)
-/// `ApplicationLogger` is a class that provides logging functionalities for Linux and Windows operating systems.
-open class ApplicationLogger {
-    /// Prints a debug message.
-    /// - Parameter message: The message to be logged.
-    open func debug(_ message: String) {
-        debug { message }
+/// `ApplicationLogger` is a struct that provides logging functionalities for Linux and Windows operating systems using closures.
+public struct ApplicationLogger: Sendable {
+    private var debugClosure: @Sendable (String) -> Void
+    private var errorClosure: @Sendable (Error, String?) -> Void
+
+    /// Initializes the `ApplicationLogger` struct with custom behaviors for each closure.
+    /// - Parameters:
+    ///   - debug: A closure for logging debug messages.
+    ///   - debugWithClosure: A closure for logging debug messages using a closure.
+    ///   - errorWithMessage: A closure for logging error messages with an optional custom message.
+    ///   - errorWithString: A closure for logging error messages as strings.
+    public init(
+        debug: @Sendable @escaping (String) -> Void = { print($0) },
+        error: @Sendable @escaping (Error, String?) -> Void = { error, message in
+            if let message = message {
+                print("\(message) (Error: \(error.localizedDescription))")
+            } else {
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+    ) {
+        self.debugClosure = debug
+        self.errorClosure = error
     }
 
     /// Prints a debug message.
+    /// - Parameter message: The message to be logged.
+    public func debug(_ message: String) {
+        debugClosure(message)
+    }
+
+    /// Prints a debug message using a closure.
     /// - Parameter message: A closure that returns the message to be logged.
-    open func debug(_ message: () -> String) {
-        print(message())
+    public func debug(_ message: () -> String) {
+        debug(message())
     }
 
     /// Logs an error message.
     /// - Parameters:
     ///   - error: The error that occurred.
     ///   - message: An optional custom message to accompany the error.
-    open func error(_ error: Error, message: String? = nil) {
-        guard let message else {
-            return print("Error: \(error.localizedDescription)")
-        }
-
-        print("\(message) (Error: \(error.localizedDescription))")
-    }
-
-    /// Logs an error message.
-    /// - Parameter message: The error message to be logged.
-    open func error(_ message: String) {
-        print("Error: \(message)")
+    public func error(_ error: Error, message: String? = nil) {
+        errorClosure(error, message)
     }
 }
+
 #endif
