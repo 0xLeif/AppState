5e2af62bb0a83fefbc197807f83570108ff8f1ed
Leif <zmeriksen@icloud.com>
2024-09-19 18:58:08 -0600

    Attempt to fix all tests for linux and windows

--- Tests/AppStateTests/KeychainTests.swift
+++ Tests/AppStateTests/KeychainTests.swift
@@ -1,71 +1,76 @@
-//#if !os(Linux) && !os(Windows)
-//import XCTest
-//@testable import AppState
-//
-//@MainActor
-//final class KeychainTests: XCTestCase {
-//    func testKeychainInitKeys() throws {
-//        let keychain = Keychain(keys: ["key"])
-//
-//        XCTAssertThrowsError(try keychain.resolve("key"))
-//    }
-//
-//    func testKeychainInitValues() throws {
-//        let keychain = Keychain(keys: ["key"])
-//
-//        keychain.set(value: "abc", forKey: "key")
-//
-//        let value = try keychain.resolve("key")
-//
-//        XCTAssertEqual(value, "abc")
-//
-//        keychain.remove("key")
-//    }
-//
-//    func testKeychainContains() throws {
-//        let keychain = Keychain()
-//
-//        XCTAssertFalse(keychain.contains("key"))
-//
-//        keychain.set(value: "abc", forKey: "key")
-//
-//        XCTAssertTrue(keychain.contains("key"))
-//
-//        keychain.remove("key")
-//    }
-//
-//    func testKeychainRequiresSuccess() throws {
-//        let keychain = Keychain(keys: ["key"])
-//
-//        keychain.set(value: "abs", forKey: "key")
-//
-//        XCTAssertNoThrow(try keychain.require("key"))
-//
-//        keychain.remove("key")
-//    }
-//
-//    func testKeychainRequiresFailure() throws {
-//        let keychain = Keychain()
-//
-//        XCTAssertThrowsError(try keychain.require("key"))
-//    }
-//
-//    func testKeychainValues() throws {
-//        let keychain = Keychain(keys: ["key"])
-//
-//        keychain.set(value: "abc", forKey: "key")
-//
-//        let values = keychain.values()
-//        let secureValue = keychain.get("key")
-//
-//        XCTAssertEqual(values.count, 1)
-//        XCTAssertNotNil(secureValue)
-//        XCTAssertEqual(secureValue, "abc")
-//
-//        keychain.remove("key")
-//
-//        XCTAssertEqual(keychain.values().count, 0)
-//        XCTAssertNil(keychain.get("key"))
-//    }
-//}
-//#endif
+#if !os(Linux) && !os(Windows)
+import XCTest
+@testable import AppState
+
+final class KeychainTests: XCTestCase {
+    @MainActor
+    func testKeychainInitKeys() async throws {
+        let keychain = Keychain(keys: ["key"])
+
+        XCTAssertThrowsError(try keychain.resolve("key"))
+    }
+
+    @MainActor
+    func testKeychainInitValues() async throws {
+        let keychain = Keychain(keys: ["key"])
+
+        keychain.set(value: "abc", forKey: "key")
+
+        let value = try keychain.resolve("key")
+
+        XCTAssertEqual(value, "abc")
+
+        keychain.remove("key")
+    }
+
+    @MainActor
+    func testKeychainContains() async throws {
+        let keychain = Keychain()
+
+        XCTAssertFalse(keychain.contains("key"))
+
+        keychain.set(value: "abc", forKey: "key")
+
+        XCTAssertTrue(keychain.contains("key"))
+
+        keychain.remove("key")
+    }
+
+    @MainActor
+    func testKeychainRequiresSuccess() async throws {
+        let keychain = Keychain(keys: ["key"])
+
+        keychain.set(value: "abs", forKey: "key")
+
+        XCTAssertNoThrow(try keychain.require("key"))
+
+        keychain.remove("key")
+    }
+
+    @MainActor
+    func testKeychainRequiresFailure() async throws {
+        let keychain = Keychain()
+
+        XCTAssertThrowsError(try keychain.require("key"))
+    }
+
+    @MainActor
+    func testKeychainValues() async throws {
+        let keychain = Keychain(keys: ["key"])
+
+        keychain.set(value: "abc", forKey: "key")
+
+        let values = keychain.values()
+        let secureValue = keychain.get("key")
+
+        XCTAssertEqual(values.count, 1)
+        XCTAssertNotNil(secureValue)
+        XCTAssertEqual(secureValue, "abc")
+
+        keychain.remove("key")
+
+        XCTAssertEqual(keychain.values().count, 0)
+        XCTAssertNil(keychain.get("key"))
+    }
+}
+#endif
--- Tests/AppStateTests/ObservedDependencyTests.swift
+++ Tests/AppStateTests/ObservedDependencyTests.swift
@@ -1,59 +1,61 @@
-//#if !os(Linux) && !os(Windows)
-//import SwiftUI
-//import XCTest
-//@testable import AppState
-//
-//@MainActor
-//fileprivate class ObservableService: ObservableObject {
-//    @Published var count: Int
-//
-//    init() {
-//        count = 0
-//    }
-//}
-//
-//fileprivate extension Application {
-//    var test: Dependency<String> {
-//        dependency("!!!")
-//    }
-//
-//    @MainActor
-//    var observableService: Dependency<ObservableService> {
-//        dependency(ObservableService())
-//    }
-//}
-//
-//@MainActor
-//fileprivate struct ExampleDependencyWrapper {
-//    @ObservedDependency(\.observableService) var service
-//
-//    func test() {
-//        service.count += 1
-//
-//        _ = Picker("", selection: $service.count, content: EmptyView.init)
-//    }
-//}
-//
-//@MainActor
-//final class ObservedDependencyTests: XCTestCase {
-//    override func setUp() async throws {
-//        Application.logging(isEnabled: true)
-//    }
-//
-//    override func tearDown() async throws {
-//        let applicationDescription = Application.description
-//
-//        Application.logger.debug("ObservedDependencyTests \(applicationDescription)")
-//    }
-//
-//    func testDependency() {
-//        let example = ExampleDependencyWrapper()
-//
-//        XCTAssertEqual(example.service.count, 0)
-//
-//        example.test()
-//
-//        XCTAssertEqual(example.service.count, 1)
-//    }
-//}
-//#endif
+#if !os(Linux) && !os(Windows)
+import SwiftUI
+import XCTest
+@testable import AppState
+
+@MainActor
+fileprivate class ObservableService: ObservableObject {
+    @Published var count: Int
+
+    init() {
+        count = 0
+    }
+}
+
+fileprivate extension Application {
+    var test: Dependency<String> {
+        dependency("!!!")
+    }
+
+    @MainActor
+    var observableService: Dependency<ObservableService> {
+        dependency(ObservableService())
+    }
+}
+
+@MainActor
+fileprivate struct ExampleDependencyWrapper {
+    @ObservedDependency(\.observableService) var service
+
+    func test() {
+        service.count += 1
+
+        _ = Picker("", selection: $service.count, content: EmptyView.init)
+    }
+}
+
+final class ObservedDependencyTests: XCTestCase {
+    @MainActor
+    override func setUp() async throws {
+        Application.logging(isEnabled: true)
+    }
+
+    @MainActor
+    override func tearDown() async throws {
+        let applicationDescription = Application.description
+
+        Application.logger.debug("ObservedDependencyTests \(applicationDescription)")
+    }
+
+    @MainActor
+    func testDependency() async {
+        let example = ExampleDependencyWrapper()
+
+        XCTAssertEqual(example.service.count, 0)
+
+        example.test()
+
+        XCTAssertEqual(example.service.count, 1)
+    }
+}
+#endif
