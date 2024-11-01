366c2374b092faa2e14b85855df9ba8e1ab9d500
Leif <zmeriksen@icloud.com>
2024-11-01 17:20:16 -0600

    Fix error about property not on main actor

--- Sources/AppState/PropertyWrappers/Dependency/ObservedDependency.swift
+++ Sources/AppState/PropertyWrappers/Dependency/ObservedDependency.swift
@@ -2,6 +2,7 @@
 import SwiftUI
 
 /// The `@ObservedDependency` property wrapper is a feature provided by AppState, intended to simplify dependency handling throughout your application. It makes it easy to access, share, and manage dependencies in a neat and Swift idiomatic way. It works the same as `@AppDependency`, but comes with the power of the `@ObservedObject` property wrapper.
+@MainActor
 @propertyWrapper public struct ObservedDependency<Value: Sendable>: DynamicProperty where Value: ObservableObject {
     /// Path for accessing `ObservedDependency` from Application.
     private let keyPath: KeyPath<Application, Application.Dependency<Value>>
